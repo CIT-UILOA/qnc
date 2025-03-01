64dcc755a4340d7be304ab1aab0cfc84b3638ebd
Kiwifuit <75169058+Kiwifuit@users.noreply.github.com>
2025-03-01 17:32:12 +0800

    feat: read loop

--- src/main.c
+++ src/main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <string.h>
-#define BOARD_SIZE 9
+#define BOARD_SIZE 10
 
 int main()
 {
   FILE *input = fopen("input", "r");
+  if (input == NULL)
+  {
+    perror("Unable to open input file");
+    return 1;
+  }
+
   char buf[BOARD_SIZE];
   memset(&buf, 0, BOARD_SIZE);
 
-  fgets(&buf, BOARD_SIZE, input);
+  int lineno = 0;
+  while (fgets(buf, BOARD_SIZE, input) != NULL)
+  {
+    size_t len = strlen(buf);
+    if (len > 0 && buf[len - 1] == '\n')
+    {
+      buf[len - 1] = '\0';
+    }
+    printf("%d %s\n", lineno++, buf);
+  };
 
   fclose(input);
 }
